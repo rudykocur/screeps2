0c2bb9612268c3d319aaa779db73916f2144ca03
rudykocur <grzegorz.przydryga@gmail.com>
2017-10-27 09:35:52 +0200

    additional usefull targets when on "idle"

--- src/room.labs.js
+++ src/room.labs.js
@@ -232,15 +232,19 @@ class LabManager extends utils.Executable {
                 {resource: RESOURCE_CATALYZED_GHODIUM_ALKALIDE, amount: 3000},
                 {resource: RESOURCE_CATALYZED_ZYNTHIUM_ALKALIDE, amount: 3000},
                 {resource: RESOURCE_CATALYZED_UTRIUM_ACID, amount: 3000},
+                {resource: RESOURCE_HYDROXIDE, amount: 5000},
+                {resource: RESOURCE_GHODIUM, amount: 5000},
             ];
         }
 
         return [
-                {resource: RESOURCE_LEMERGIUM_ALKALIDE, amount: 3000},
-                {resource: RESOURCE_GHODIUM_ALKALIDE, amount: 3000},
-                {resource: RESOURCE_ZYNTHIUM_ALKALIDE, amount: 3000},
-                {resource: RESOURCE_CATALYZED_UTRIUM_ACID, amount: 3000},
-            ];
+            {resource: RESOURCE_LEMERGIUM_ALKALIDE, amount: 3000},
+            {resource: RESOURCE_GHODIUM_ALKALIDE, amount: 3000},
+            {resource: RESOURCE_ZYNTHIUM_ALKALIDE, amount: 3000},
+            {resource: RESOURCE_CATALYZED_UTRIUM_ACID, amount: 3000},
+            {resource: RESOURCE_HYDROXIDE, amount: 5000},
+            {resource: RESOURCE_GHODIUM, amount: 5000},
+        ];
     }
 
     regenerateLabLayout() {
